1900a1a03438b9369af3a7620aaef906deb554bc
Lean <tales-of@live.de>
2020-01-28 19:43:42 +0100

    Add japanese version

---
+++ seedchecker/common.js
@@ -0,0 +1,267 @@
+        var EventHash = bigInt("1721953670860364124");
+
+        var Nests = [[16, 52, 12], [37, 64, 12], [31, 90, 12], [03, 51, 12], [04, 46, 12], [33, 62, 12], [39, 65, 12], [01, 48, 12], [31, 90, 12], [26, 59, 02], [28, 79, 02], [26, 58, 02], [04, 92, 02], [26, 59, 02], [02, 50, 15], [06, 47, 15], [00, 00, 15], [07, 44, 04], [39, 65, 04], [31, 61, 04], [31, 61, 04], [09, 75, 16], [09, 75, 16], [44, 42, 16], [07, 44, 16], [09, 91, 16], [35, 63, 16], [38, 64, 00], [08, 91, 13], [41, 76, 13], [41, 76, 13], [05, 46, 13], [01, 43, 13], [15, 51, 07], [12, 45, 07], [11, 49, 07], [15, 83, 07], [11, 84, 07], [10, 48, 11], [41, 76, 11], [29, 60, 11], [41, 76, 11], [29, 60, 11], [29, 86, 04], [10, 48, 11], [14, 50, 10], [30, 60, 10], [24, 56, 10], [40, 65, 10], [34, 82, 01], [13, 87, 01], [08, 44, 01], [16, 52, 01], [40, 65, 01], [04, 92, 01], [05, 46, 01], [36, 63, 01], [27, 59, 01], [40, 66, 14], [01, 81, 14], [03, 51, 14], [19, 80, 14], [06, 43, 14], [27, 59, 14], [02, 50, 14], [12, 85, 14], [22, 55, 14], [14, 50, 14], [18, 54, 14], [35, 63, 14], [17, 93, 03], [17, 52, 03], [15, 93, 03], [09, 75, 03], [11, 49, 03], [10, 48, 03], [21, 55, 03], [20, 54, 03], [28, 43, 06], [36, 88, 03], [42, 67, 06], [36, 69, 06], [27, 70, 06], [25, 72, 06], [12, 45, 08], [32, 61, 08], [30, 57, 08], [19, 53, 08], [23, 73, 08], [33, 62, 08], [14, 68, 08], [30, 60, 05], [23, 55, 05], [18, 71, 05], [32, 89, 05], [24, 78, 05], [25, 56, 09], [18, 77, 09], [08, 44, 09], [34, 74, 09]]
+           
+            var ShinyType = ["-", "☆", "◇"]
+            var GenderRatios = [255, 31, 31, 31, 31, 31, 31, 31, 31, 31, 127, 127, 127, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 127, 127, 255, 255, 255, 255, 255, 255, 255, 255, 191, 191, 191, 191, 255, 255, 255, 255, 127, 127, 127, 255, 255, 255, 255, 127, 127, 127, 127, 255, 255, 255, 255, 63, 63, 255, 255, 255, 255, 255, 255, 63, 63, 63, 255, 255, 255, 255, 255, 255, 255, 255, 127, 127, 255, 255, 255, 255, 127, 255, 255, 255, 255, 255, 255, 127, 127, 127, 127, 127, 127, 255, 255, 127, 127, 255, 255, 255, 255, 255, 255, 0, 0, 255, 127, 127, 127, 127, 255, 255, 255, 255, 255, 127, 127, 255, 255, 127, 255, 255, 255, 255, 255, 255, 127, 127, 127, 255, 31, 31, 31, 31, 255, 255, 255, 255, 255, 255, 31, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 127, 127, 255, 255, 255, 255, 255, 127, 127, 127, 191, 255, 31, 31, 127, 127, 255, 255, 255, 127, 255, 255, 127, 255, 255, 255, 255, 255, 255, 255, 255, 127, 127, 31, 31, 255, 255, 255, 255, 127, 255, 255, 255, 255, 255, 127, 255, 255, 127, 255, 127, 255, 127, 255, 255, 255, 255, 127, 127, 191, 127, 127, 127, 127, 255, 255, 255, 255, 255, 255, 255, 255, 255, 0, 0, 255, 255, 255, 255, 255, 255, 255, 255, 127, 127, 127, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 127, 127, 255, 255, 255, 255, 255, 127, 127, 127, 127, 127, 127, 255, 255, 127, 127, 127, 127, 127, 255, 255, 255, 255, 255, 255, 255, 127, 127, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 127, 127, 255, 255, 255, 255, 255, 127, 127, 255, 255, 255, 255, 127, 255, 255, 255, 255, 127, 127, 255, 255, 127, 255, 255, 255, 127, 127, 127, 255, 255, 255, 255, 255, 255, 255, 255, 127, 127, 127, 127, 255, 255, 255, 255, 255, 255, 127, 127, 255, 255, 255, 255, 127, 127, 255, 255, 255, 127, 127, 127, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 127, 127, 255, 255, 255, 255, 255, 255, 255, 31, 254, 255, 255, 255, 127, 127, 127, 127, 255, 127, 127, 255, 255, 255, 255, 255, 255, 255, 127, 127, 255, 255, 127, 127, 255, 255, 255, 255, 255, 255, 31, 31, 31, 127, 127, 127, 127, 127, 127, 255, 255, 255, 127, 127, 127, 127, 255, 255, 127, 255, 255, 255, 31, 255, 31, 31, 255, 127, 255, 0, 255, 127, 254, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 127, 127, 255, 255, 255, 255, 255, 255, 127, 127, 127, 127, 127, 255, 255, 127, 127, 127, 127, 127, 127, 127, 255, 63, 63, 63, 127, 127, 127, 0, 0, 255, 255, 255, 255, 255, 255, 127, 127, 255, 255, 127, 255, 255, 255, 127, 127, 127, 127, 127, 127, 127, 127, 127, 127, 255, 255, 255, 255, 127, 127, 255, 255, 191, 191, 191, 191, 191, 127, 127, 127, 255, 255, 127, 127, 127, 255, 255, 255, 127, 127, 255, 255, 127, 127, 255, 127, 127, 127, 127, 255, 255, 255, 255, 255, 255, 127, 127, 127, 127, 127, 127, 127, 127, 127, 127, 255, 127, 127, 127, 255, 255, 255, 255, 255, 127, 127, 255, 0, 0, 254, 254, 127, 127, 127, 127, 127, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 127, 127, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 127, 127, 255, 127, 0, 127, 127, 127, 127, 127, 127, 127, 127, 127, 127, 127, 255, 255, 255, 255, 127, 127, 255, 255, 255, 255, 31, 127, 255, 255, 127, 127, 127, 255, 127, 127, 127, 127, 127, 127, 127, 127, 255, 255, 255, 255, 255, 255, 31, 31, 31, 31, 31, 31, 31, 31, 31, 255, 255, 255, 255, 255, 127, 127, 127, 255, 255, 255, 127, 127, 255, 255, 127, 127, 127, 127, 127, 127, 127, 255, 255, 127, 127, 31, 254, 127, 127, 254, 254, 254, 255, 127, 127, 127, 127, 255, 255, 127, 255, 255, 255, 255, 127, 127, 127, 255, 127, 255, 127, 127, 127, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 31, 31, 31, 31, 31, 31, 31, 31, 31, 127, 127, 127, 127, 127, 127, 127, 127, 127, 127, 127, 127, 127, 127, 127, 127, 127, 127, 127, 127, 127, 127, 127, 127, 127, 127, 127, 127, 127, 127, 127, 127, 127, 127, 127, 255, 255, 254, 254, 254, 0, 0, 0, 127, 127, 191, 127, 127, 127, 254, 254, 255, 127, 127, 127, 127, 127, 0, 127, 127, 127, 255, 255, 255, 255, 127, 127, 127, 127, 255, 255, 255, 255, 255, 0, 0, 0, 0, 0, 0, 0, 127, 255, 255, 191, 191, 127, 127, 127, 127, 127, 255, 255, 255, 127, 127, 127, 255, 255, 255, 255, 127, 127, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 191, 127, 127, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 127, 127, 127, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 127, 127, 127, 127, 127, 127, 255, 255, 255, 255, 255, 255, 127, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 254, 127, 127, 127, 127, 127, 127, 127, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 127, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 127, 255, 255, 255, 127, 127, 127, 255, 255, 254, 254, 254, 254, 254, 254, 254, 254, 127, 254, 127, 255, 255, 255]
+            var ToxtricityAmplifiedNatures = [ 0x03, 0x04, 0x02, 0x08, 0x09, 0x13, 0x16, 0x0B, 0x0D, 0x0E, 0x18 ];
+            var ToxtricityLowKeyNatures = [ 0x01, 0x05, 0x07, 0x0A, 0x0C, 0x0F, 0x10, 0x11, 0x12, 0x14, 0x15, 0x17 ];
+            var s0 = 0
+            var s1 = 0
+            var XC = bigInt("82A2B175229D6A5B", 16)
+
+            function rotl(x, k) {
+                var a = x.shiftLeft(k) 
+                a = a.and(MASK)
+                var b = x.shiftRight(bigInt("64").minus(k))
+                return a.or(b)
+            }
+            
+            function next() {
+                var res = s0.plus(s1) 
+                res = res.and(MASK)
+                s1 = s0.xor(s1)
+                s0 = rotl(s0, bigInt("24")).xor(s1).xor((s1.shiftLeft(bigInt("16"))).and(MASK))
+                s1 = rotl(s1, bigInt("37"))
+                return res 
+            }
+            
+            function setSeed(x) {
+                s0 = x 
+                s1 = XC
+            }
+            
+            function nextInt(num, mask) {
+                var s = next().and(mask)
+                while(s.compare(num) >= 0) {
+                    s = next().and(mask)
+                }
+                return Number(s);
+            }
+            
+            function GetShinyValue(n) {
+                return GetShinyXor(n) >>> 4
+            }
+            
+            function GetShinyXor(n) {
+                return (n >>> 16) ^ (n&0xFFFF)
+            }
+            
+            function GetShinyType(pid, tidsid) {
+                var p = GetShinyXor(pid);
+                var t = GetShinyXor(tidsid);
+                if (p == t)
+                    return 2; // square;
+                if ((p ^ t) < 0x10)
+                    return 1; // star
+                return 0;
+            }
+
+            function getData(seed, idx, pkmn) {
+                setSeed(seed)
+                var ec = nextInt(SMASK, SMASK)
+                var sidtid = nextInt(SMASK, SMASK)
+                var pid = nextInt(SMASK, SMASK)
+                var shiny = GetShinyType(pid, sidtid)
+                var iv = [-1, -1, -1, -1, -1, -1]
+                var i = 0
+                while(i < pkmn["FlawlessIVs"]) {
+                    var s = nextInt(bigInt("6"), bigInt("7"))
+                    if (iv[s] == -1) {
+                        i += 1
+                        iv[s] = 31
+                    }
+                }
+                for(i = 0; i < 6; i++) {
+                    if(iv[i] == -1) {
+                        iv[i] = nextInt(bigInt("32"), bigInt("31"))
+                    }
+                }
+                var ability = 0;
+                var abilityNames = ["1", "2", "H"]
+                if(pkmn["Ability"] < 3) {
+                    ability = pkmn["Ability"]
+                } else {
+                    if(pkmn["Ability"] == 3) {
+                        ability = abilityNames[nextInt(bigInt("2"), bigInt("1"))];
+                    } else {
+                        ability = abilityNames[nextInt(bigInt("3"), bigInt("3"))];
+                    }
+                }
+                
+                var gt = GenderRatios[pkmn["Species"]]
+                var gender = 0
+                if(gt == 255) {
+                    gender = Gender[2]
+                } else if (gt == 254) {
+                    gender = Gender[1]
+                } else if (gt == 0) {
+                    gender = Gender[0]
+                } else {
+                    gender = Gender[nextInt(bigInt("253"), bigInt("255")) + 1 < gt? 1 : 0]
+                }
+                
+                var nature;
+                if(pkmn["Species"] == 849) {
+                    if (pkmn["AltForm"] == 0) {
+                        nature = ToxtricityAmplifiedNatures[nextInt(bigInt(ToxtricityAmplifiedNatures.length), bigInt("31"))]
+                    } else {
+                        nature = ToxtricityLowKeyNatures[nextInt(bigInt(ToxtricityLowKeyNatures.length), bigInt("31"))]
+                    }
+                } else {
+                    nature = nextInt(bigInt("25"), bigInt("31"));
+                }
+                var res = {"Shiny": ShinyType[shiny], "HP": iv[0], "ATK": iv[1], "DEF": iv[2], "SPA": iv[3], "SPD": iv[4], "SPE": iv[5], "Nature": Nature[nature], "Gender": gender, "Ability": ability, "Seed": seed, "Index": idx}
+                return res
+            }
+
+            var MASK = bigInt("FFFFFFFFFFFFFFFF", 16)
+            var SMASK = bigInt("FFFFFFFF", 16)
+            var start = 0
+            var end = 100
+            
+            function advanceFrame(seed, frame) {
+                for(var i=0; i<frame; i++) {
+                    seed = seed.plus(XC)
+                    seed = seed.and(MASK)
+                }
+                return seed;
+            }
+			
+            function load_nests() {
+                for(var i = 1; i <= 96; i++) {
+					$("#nest").append(new Option(nestName + " " + i, i-1));
+                }
+            }
+
+            var entries = []
+            
+			function get_nest(den, rarity) {
+                data = den.split(",")
+                nestdata = [Number(data[0]), Number(data[1])]
+                nestID = nestdata[rarity] - 1
+				$("#nest").val(nestID);
+				return nestID 
+			}
+			
+            function change_species(nestID, game) {
+                $("#species").children().remove().end()
+                $.getJSON("https://leanny.github.io/seedchecker/nest" + game + ".json", function(data) {
+                    entries = data[nestID]["Entries"]
+                    $.each(data[nestID]["Entries"], function(idx, entry) {
+                        var minRank = Number(entry["MinRank"]) + 1
+                        var maxRank = Number(entry["MaxRank"]) + 1
+                        var gmax = ""
+                        if(entry["IsGigantamax"]) gmax = "(G-Max) "
+                        var rank = "" + minRank + "-" + maxRank
+                        if(minRank == maxRank) rank = "" + minRank
+                        $("#species").append(new Option(Names[entry["Species"]] + " " + gmax + " " + rank + " \u2605", entry))
+                    });
+                });
+            }
+            
+            function calculateShinyFrame() {
+                var str = $("#seedbox").val();
+                try {
+                    seed = bigInt(str, 16)
+                } catch(e) {
+                    alert("Invalid Seed")
+                    return;
+                }
+                var minstr = $("#startframe").val();
+                start = parseInt(minstr)
+                if(isNaN(start)) {
+                    alert("Invalid Start Frame")
+                    return;
+                }
+                var maxstr = $("#endframe").val();
+                limit = parseInt(maxstr)
+                if(isNaN(end)) {
+                    alert("Invalid Number of Frames")
+                    return;
+                }
+                var data = []
+                var result = []
+                seed = advanceFrame(seed, start)
+                end = start + limit 
+                for(i = start;; i++) {
+                    res = getData(seed, i, entries[$("#species")[0].selectedIndex])
+                    seed = seed.plus(XC)
+                    seed = seed.and(MASK)
+                    if(res["Shiny"] != "-"){
+                        result.push(res)
+                        break
+                    }
+                }
+                $.each(result, function(idx, res) {
+                    var row = ({
+                        Frame: res["Index"],
+                        HP:  res["HP"],
+                        ATK: res["ATK"],
+                        DEF: res["DEF"],
+                        SPA: res["SPA"],
+                        SPD: res["SPD"],
+                        SPE: res["SPE"],
+                        Shiny: res["Shiny"],
+                        Nature: res["Nature"],
+                        Gender: res["Gender"],
+                        Ability: res["Ability"],
+                        Seed: res["Seed"].toString(16)
+                    });
+                    data.push(row);
+                });
+                $('#seed-table').bootstrapTable('refreshOptions', {
+                    "data": data
+                });
+            }
+
+            function calculateFrames() {
+                var str = $("#seedbox").val();
+                try {
+                    seed = bigInt(str, 16)
+                } catch(e) {
+                    alert("Invalid Seed")
+                    return;
+                }
+                var minstr = $("#startframe").val();
+                start = parseInt(minstr)
+                if(isNaN(start)) {
+                    alert("Invalid Start Frame")
+                    return;
+                }
+                var maxstr = $("#endframe").val();
+                limit = parseInt(maxstr)
+                if(isNaN(end)) {
+                    alert("Invalid Number of Frames")
+                    return;
+                }
+                var data = []
+                var result = []
+                seed = advanceFrame(seed, start)
+                end = start + limit 
+                for(i = start; i < end; i++) {
+                    result.push(getData(seed, i, entries[$("#species")[0].selectedIndex]))
+                    seed = seed.plus(XC)
+                    seed = seed.and(MASK)
+                }
+                $.each(result, function(idx, res) {
+                    var row = ({
+                        Frame: res["Index"],
+                        HP:  res["HP"],
+                        ATK: res["ATK"],
+                        DEF: res["DEF"],
+                        SPA: res["SPA"],
+                        SPD: res["SPD"],
+                        SPE: res["SPE"],
+                        Shiny: res["Shiny"],
+                        Nature: res["Nature"],
+                        Gender: res["Gender"],
+                        Ability: res["Ability"],
+                        Seed: res["Seed"].toString(16)
+                    });
+                    data.push(row);
+                });
+                $('#seed-table').bootstrapTable('refreshOptions', {
+                    "data": data
+                });
+            }
